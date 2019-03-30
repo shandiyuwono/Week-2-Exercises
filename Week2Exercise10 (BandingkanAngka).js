48afeb0cacc17be8c3ec92c04dc9f9847d006981
shandiyuwono <48650296+shandiyuwono@users.noreply.github.com>
2019-03-30 21:13:07 +0700

    Add files via upload

--- Week2Exercise10 (BandingkanAngka).js
+++ Week2Exercise10 (BandingkanAngka).js
@@ -1,4 +1,3 @@
-// question 1
 
 function bandingkanAngka(angka1, angka2) {
     if(angka2 > angka1) {
@@ -12,27 +11,4 @@ function bandingkanAngka(angka1, angka2) {
   
   console.log(bandingkanAngka(5, 3))
 
-  // question 2
 
-function balikKata(kata) {
-  var kata2 = ""
-  for(i = kata.length -1; i >= 0; i--){
-    kata2 += kata[i]
-  }
-  return kata2
-}
-
-console.log(balikKata('John Doe'))
-
-// question 3
-
-function konversiMenit(menit) {
-    var jam = menit / 60;
-    var tjam = Math.floor(jam);
-    menit = (jam - tjam) * 60;
-    tmenit = Math.round(menit)
-    if(tmenit < 10) {
-      return tjam + ":" + "0" + tmenit
-    }
-    return tjam + ":" + tmenit
-  }
