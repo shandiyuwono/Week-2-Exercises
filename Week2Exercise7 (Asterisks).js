22957fa8f202a109fe9adbc97ec41c0562c598df
shandiyuwono <48650296+shandiyuwono@users.noreply.github.com>
2019-03-30 19:57:43 +0700

    Add files via upload

--- Week2Exercise7 (Asterisks).js
+++ Week2Exercise7 (Asterisks).js
@@ -8,11 +8,24 @@ for(var i = 1; i <= rows1; i++) {
 
 // question 2
 
+var rows2 = 5
+
+for(var i = 1; i <= rows2; i++) {
+  var output = ""
+  for(var j = 1; j <= rows2; j++) {
+    output += "*"
+  } 
+  console.log(output)
+}
+
 // question 3
 
 var rows1 = 5
-asterix = ""
 
-for(var i = 1; i <= rows1; i++) {
-  console.log(asterix += "*")
+for(var j = 1; j <= rows1; j++) {
+  var output = ""
+  for(var i = 1; i <= j; i++) {
+      output += "*"
+    } 
+  console.log(output)
 }
